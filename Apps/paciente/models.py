165b2dc6ae907b2a318c8b69d6287166907f6e06
emilio4585 <emiliosalas54@gmail.com>
2020-08-10 16:56:50 -0500

    Commit de seguridad

---
+++ Apps/paciente/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class paciente(models.Model):
+    nombre
+    edad
+    estado_civil
+    feche_nacimiento
+    lugar_origen
+    actividad
+    descripcion
+    compañia
+    estado_fisico
