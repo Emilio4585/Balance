7b7729fac6ce00d05549674e67dba7f229aa43f8
emilio4585 <emiliosalas54@gmail.com>
2020-08-17 21:16:16 -0500

    Added new tests in posts

--- Apps/posts/tests.py
+++ Apps/posts/tests.py
@@ -1,10 +1,34 @@
 from django.test import TestCase, TransactionTestCase
+from .models import Post
+from django.urls import reverse
 
 
 # Create your tests here.
 
 
-class PagesTests(TestCase):
-    def test_home_post_status_code(self):
-        response = self.client.get('/posts/')
-        self.assertEqual(response.status_code, 200)
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Just a text")
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'Just a text')
+
+
+class HomePageView(TestCase):
+    def setUp(self):
+        Post.objects.create(text='This is another text')
+
+    def text_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/posts/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'posts/main.html')
